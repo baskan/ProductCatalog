1e667cb206cb5ab890e1a658d05c15769d9b8733
David Thorpe <davzie@davzie.com>
2013-06-24 10:31:26 +0100

    Created Migrations For The User's Table
    Signed-off-by: David Thorpe <davzie@davzie.com>

---
+++ src/migrations/2013_06_24_092625_create_users_table.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsersTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		if ( !Schema::hasTable('users') )
+		{
+			Schema::create('users', function($table)
+			{
+
+				$table->engine = 'InnoDB';
+			    $table->increments('id');
+
+			    $table->string('email',255);
+			    $table->string('password',255);
+			    $table->unique('email');
+
+			    $table->string('first_name',255);
+			    $table->string('last_name',255);
+			    $table->boolean('is_admin')->default(false);
+			    $table->dateTime('last_login');
+			    $table->timestamps();
+
+			});
+		}
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('users');
+	}
+
+}
