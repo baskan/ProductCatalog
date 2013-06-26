0becf94cfbe184ec17f62cf3c6cf1015b0cfd900
David Thorpe <davzie@davzie.com>
2013-06-26 10:10:09 +0100

    Messaging Partial Now Has Prettier Headings
    Also removed the paragraph margins
    
    Signed-off-by: David Thorpe <davzie@davzie.com>

--- src/views/partials/messaging.blade.php
+++ src/views/partials/messaging.blade.php
@@ -1,12 +1,14 @@
 @if( $errors->all() )
     <div class="alert alert-error">
-    @foreach ($errors->all('<p>:message</p>') as $msg)
-        {{ $msg }}
-    @endforeach
+        <p><strong>Whoops! There was a problem.</strong></p>
+        @foreach ($errors->all('<p>:message</p>') as $msg)
+            {{ $msg }}
+        @endforeach
     </div>
 @endif
 @if( $success )
     <div class="alert alert-success">
+        <p><strong>Success!</strong></p>
         {{ $success }}
     </div>
 @endif
