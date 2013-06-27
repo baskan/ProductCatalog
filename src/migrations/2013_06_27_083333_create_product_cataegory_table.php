5f0d596c7ca8b54b8ba1e3f5b2e77c3ea179f747
David Thorpe <davzie@davzie.com>
2013-06-27 10:09:01 +0100

    Product Category Pivot Table Migration
    Foreign keys included, like magic.
    
    Signed-off-by: David Thorpe <davzie@davzie.com>

---
+++ src/migrations/2013_06_27_083333_create_product_cataegory_table.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProductCataegoryTable extends Migration {
+
+/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		if ( !Schema::hasTable('product_categories') )
+		{
+			Schema::create('product_categories', function($table)
+			{
+
+				$table->engine = 'InnoDB';
+				$table->increments('id');
+			    $table->integer('product_id')->unsigned();
+			    $table->integer('category_id')->unsigned();
+			    $table->timestamps();
+
+				$table->foreign('product_id')
+					->references('id')->on('products')
+					->onDelete('cascade');
+
+				$table->foreign('category_id')
+					->references('id')->on('categories')
+					->onDelete('cascade');
+			});
+		}
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('product_categories');
+	}
+
+}
