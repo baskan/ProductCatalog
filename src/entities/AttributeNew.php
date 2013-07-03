3ab9fde9dd9584b609048e8d113b8d4f83e8363a
David Thorpe <davzie@davzie.com>
2013-07-03 14:39:30 +0100

    Validate The Attribute Keys Better
    Signed-off-by: David Thorpe <davzie@davzie.com>

--- src/entities/AttributeNew.php
+++ src/entities/AttributeNew.php
@@ -7,7 +7,7 @@ class AttributeNew extends Base {
 
     protected static $rules = [
         'attribute_type_id'     =>  'required|integer|exists:attribute_types,id',
-        'key'                   =>  'required|max:255|unique:attributes,key',
+        'key'                   =>  'required|max:255|alpha_dash|unique:attributes,key',
         'name'                  =>  'required|max:255|unique:attributes,name'
     ];
 
