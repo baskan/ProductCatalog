c432d8e67c61a0716169da283e59074d8073219e
David Thorpe <davzie@davzie.com>
2013-07-12 15:12:29 +0100

    Regex Improvement For Attributes
    Signed-off-by: David Thorpe <davzie@davzie.com>

--- src/Davzie/ProductCatalog/Attribute/Entities/Create.php
+++ src/Davzie/ProductCatalog/Attribute/Entities/Create.php
@@ -8,7 +8,7 @@ class Create extends Entity {
 
     protected static $rules = [
         'attribute_type_id'     =>  'required|integer|exists:attribute_types,id',
-        'key'                   =>  'required|max:255|alpha_dash|unique:attributes,key',
+        'key'                   =>  [ 'required' , 'max:255' , 'regex:/^[a-zA-Z0-9_]+([-.][a-zA-Z0-9_]+)*$/' , 'unique:attributes,key' ],
         'name'                  =>  'required|max:255|unique:attributes,name'
     ];
 
